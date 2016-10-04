744a53931abd5fb2e6a727a690c1931f0bd4a47f
Geoffrey Billy <gcb5083@psu.edu>
2016-10-04 00:12:39 -0400

    Added 10/3 changes by Saimun (prevents ghost overlap)

--- PSUPacBot.py
+++ PSUPacBot.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-
 import random
+
+#this is a simulation for the PacBot
+
 def setupArena(r,c):
     arena = [[0 for x in range(r)] for y in range(c)]
     g1 = (random.randint(0,r-1),random.randint(0,c-1))
-    g1=  (0,5)
     g2 = (random.randint(0,r-1),random.randint(0,c-1))
     g3 = (random.randint(0,r-1),random.randint(0,c-1))
     g4 = (random.randint(0,r-1),random.randint(0,c-1))
@@ -27,19 +28,18 @@ def updatePosGhost(posG,arena,mvDist,num):
     y = posG[1]
     changeX,changeY = 0,0
     arena[x][y] = u"\u2610"
-    if max(mvDist) == mvDist[0]:
+    #10/3/16 Saimun Shahee: the purpose of the second condition for each of the if statements is to check whether or not a Ghost already exists in that position
+    #hence the "(x-1,y) not in listOfGhostPositions"
+    if max(mvDist) == mvDist[0] and (x-1,y) not in listOfGhostPositions:
         changeX = -1
-    elif max(mvDist) == mvDist[2]:
+    elif max(mvDist) == mvDist[2] and (x+1,y) not in listOfGhostPositions:
         changeX = 1
-    elif max(mvDist) == mvDist[1]:
+    elif max(mvDist) == mvDist[1] and (x,y-1) not in listOfGhostPositions:
         changeY = -1
-    elif max(mvDist) == mvDist[3]:
+    elif max(mvDist) == mvDist[3] and (x,y+1) not in listOfGhostPositions:
         changeY = 1
     arena[x + changeX][y + changeY] = "G"+str(num)
-    for i in range(0,len(arena)):
-        for j in range(0,len(arena[0])):
-            print(arena[i][j],end = " ")
-        print()
+
     return arena,(x + changeX,y + changeY)
 
 def mvDist(posG,arena):
@@ -50,15 +50,24 @@ def mvDist(posG,arena):
         distList[0] = 0
     if posG[1] == 0:
         distList[1] = 0
-    if posG[0] == len(arena):
+    if posG[0] == len(arena)-1:
         distList[2] = 0       
-    if posG[1] == len(arena[0]):
+    if posG[1] == len(arena[0])-1:
         distList[3] = 0
     return distList
 
 
 board,listOfGhostPositions = setupArena(10,10)
 
-for ii in range(10):
+for i in range(10):
     print()
     board,listOfGhostPositions[0] = updatePosGhost(listOfGhostPositions[0],board,mvDist(listOfGhostPositions[0],board),1)
+    board,listOfGhostPositions[1] = updatePosGhost(listOfGhostPositions[1],board,mvDist(listOfGhostPositions[1],board),2)
+    board,listOfGhostPositions[2] = updatePosGhost(listOfGhostPositions[2],board,mvDist(listOfGhostPositions[2],board),3)
+    board,listOfGhostPositions[3] = updatePosGhost(listOfGhostPositions[3],board,mvDist(listOfGhostPositions[3],board),4)
+    #10/3/16 Saimun Shahee: I changed the location for printing the board from inside the function to outside since when you change the position of x many ghosts it
+    #unnecessarily prints the board x amount of times. So this is just for a cleaner output.
+    for i in range(0,len(board)):
+        for j in range(0,len(board[0])):
+            print(board[i][j],end = " ")
+        print()
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # robotpacman
 This repo is for the Penn State robotics team competing in the robot PacMan competition hosted by Harvard.
+
+Code written by:
+        Saimun Shahee
+        Geoffrey Billy
